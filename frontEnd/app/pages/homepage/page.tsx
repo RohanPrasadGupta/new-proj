7fb2114c45e6dd4292a7f79f6607055cc8904a1a
Rohan <rohanprasadgupta4@gmail.com>
2024-11-05 16:49:55 +0700

    update UI

--- frontEnd/app/pages/homepage/page.tsx
+++ frontEnd/app/pages/homepage/page.tsx
@@ -7,16 +7,22 @@ import toast from "react-hot-toast";
 function Page() {
   const { register, handleSubmit, reset } = useForm();
 
-  const onSubmitForm = (data: any) => {
+  const onSubmitForm = async (data: any) => {
     // console.log(data);
-    addUser(data);
-    toast.success("success");
+    const response = await addUser(data);
+    if (response.message === "User created successfully") {
+      toast.success("User created successfully");
+    } else if (response.message === "User already exists") {
+      toast.error("User already exists");
+    } else {
+      toast.error(response.message);
+    }
     // reset();
   };
 
   return (
     <>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className="bg-gray-50 dark:bg-gray-900 w-[100%] h-[100vh] items-center flex">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
